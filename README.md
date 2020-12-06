347f4fe20430e6361c55eed46a9bff3738a0c2a7
shdkej <shdkej@github.com>
2020-12-06 15:06:09 +0100

    fix: nagios

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 ---
-Odoo GVM
+# Odoo GVM
+Odoo build with docker compose
+include postgres, nginx, nagios(monitoring)
+
+## Quick start
+```
+git clone https://github.com/shdkej/odoo_gvm
+docker-compose up
+```
+
+
+## To Cloud Instance
+running github action to build ansible
+
+```
+git push origin master
+
+# run github action with ansible
+# ansible does install docker, running docker
+```
+
+## To Local Virtual Machine
+```
+cd infra
+vagrant up
+
+# check build
+vagrant ssh
+docker ps
+```
+
+## To-do
+- [ ] non stop build
+- [ ] fail recovery
--- infra/init.sh
+++ infra/init.sh
@@ -1,15 +1,19 @@
 #/bin/bash
+DOMAIN=shdkej.com
+EMAIL=shdkej@gmail.com
+
 sudo apt-get update
-sudo apt-get install -y vim openssh-server docker.io git curl
+sudo apt-get install -y vim openssh-server docker.io git curl letsencrypt
 sudo usermod -aG docker $USER
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
 sudo chmod 755 /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 
+sudo letsencrypt certonly --standalone -d $DOMAIN -m $EMAIL -n --agree-tos
+
 git clone https://github.com/shdkej/odoo_gvm
 cd odoo_gvm
 docker-compose up -d
 
 sudo crontab infra/cron
-
